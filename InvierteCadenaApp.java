66d12fd867340acc25624b2a760e2806dbeaddb3
Diciembre99 <118851645+Diciembre99@users.noreply.github.com>
2022-12-04 01:45:39 +0100

    Ejercicio3
    Sinceramente no se si es la mejor manera de hacer el ejercicio, gema dijo que debíamos hacer un for, si encuentro una solución mejor lo compartiré

--- InvierteCadenaApp.java
+++ InvierteCadenaApp.java
@@ -3,8 +3,7 @@ package es;
 import java.util.Scanner;
 
 /**
- * Esta app hace
- *
+ * Esta app que invierte las cadenas
  * @Autor Kevin Castillo
  */
 public class InvierteCadenaApp {
@@ -13,23 +12,25 @@ public class InvierteCadenaApp {
         Scanner leer = new Scanner(System.in);
         StringBuilder str = new StringBuilder();
         String texto;
-        int espacios;
         System.out.print("Introduce una cadena de caracteres:   ");
         texto = leer.nextLine();
-        
-        texto=texto.replaceAll("\\s", "");
+        //Utilizo \\s porque al utilizar replaceAll podemos hacer que elimine todos los espacios en blancos que pueda contener el string
+        texto = texto.replaceAll("\\s", "");
+        //El atributo append sirve para concaner en el StringBuilder y el .reverse invierte el contenido del objeto
         str.append(texto).reverse();
-        System.out.println("La cadena introducida sin espacios es: "+str);
-
+        System.out.println("La cadena introducida sin espacios es: " + str);
+        // toLowerCase sirve para que el texto sea representado en minusculas
         texto = texto.toLowerCase();
+        //replace se usapara sutituir los valores que tenia antes con uno nuevo, en este caso uso en length para saber cual es el tamano del string
+        //el primero inicia en cero, luego le digo que lo sustituya por una cadena vacia para que no se concatenen mas datos
+        str.replace(0, str.length(), "");
+        //Volvemos a invertir el texto 
+        str.append(texto).reverse();
+        System.out.println("La cadena introducida invertida en minúsculas es: " + str);
+        //En este caso hacemos lo mismo pero convirtiendo el texto en mayusculas antes con el UpperCase
+        texto = texto.toUpperCase();
         str.replace(0, str.length(), "");
         str.append(texto).reverse();
-        System.out.println("La cadena introducida invertida en minúsculas es: "+str);
-        
-        texto=texto.toUpperCase();
-        str.replace(0, str.length(), "");
-        str.append(texto).reverse();
-        System.out.println("La cadena introducida invertida en MAYÚSCULAS es: " +str);
-
+        System.out.println("La cadena introducida invertida en MAYÚSCULAS es: " + str);
     }
 }
