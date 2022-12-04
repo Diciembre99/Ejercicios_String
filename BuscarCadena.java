48d044290ce712705ced362c807ab3e429e8f897
Diciembre99 <118851645+Diciembre99@users.noreply.github.com>
2022-12-04 01:34:13 +0100

    Ejercicio 6
    Ejercicio 6 comentado

--- BuscarCadena.java
+++ BuscarCadena.java
@@ -3,7 +3,7 @@ package es;
 import java.util.Scanner;
 
 /**
- * Esta app hace
+ * Esta app busca la mitade de una cadena y devuelve la parte final al inicio
  *
  * @Autor Kevin Castillo
  */
@@ -11,21 +11,30 @@ public class BuscarCadena {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
+        //Declaramos las 3 cadenas a usar
         StringBuilder subcadena = new StringBuilder();
         StringBuilder subcadena1 = new StringBuilder();
         StringBuilder subcadena2 = new StringBuilder();
         System.out.print("Escribe la cadena  de caracteres: ");
         subcadena.append(leer.nextLine());
+        //Necesitamos un if que determine si una de estas cadenas termina en par
         if (BuscarCadena.Espar(subcadena1.length())) {
+            //Para saber la segunda mitad de una cadena debemos saber cual es su total de caracteres y dividirlo entre 2
             subcadena1.append(subcadena.substring(subcadena.length()/2));
         }else{
             subcadena1.append(subcadena.substring((subcadena.length()/2)+1));
         }
+        //Para escoger la primera parte de la cadena, indicamos que el indice inicial sera cero y el final la mitad del total
          subcadena2.append(subcadena.substring(0,subcadena.length()/2));
          System.out.println("La primera subcadena es "+ subcadena2);
          System.out.println("La segunda subcadena es "+subcadena1);
          System.out.println("La salida solicitada es "+subcadena1.append(subcadena2));
     }
+    /**
+     * Funcion que determina si un numero es par
+     * @param num
+     * @return Si es par o no
+     */
     public static boolean Espar(int num) {
         boolean par = false;
         if (num % 2 == 0) {
